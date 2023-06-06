72447601fd69e03f526fa906e33b7a26b8717ce9
larobernasconi <lara.bernasconi@student.supsi.ch>
2023-06-06 22:55:42 +0200

    Aggiunta ES 4

---
+++ Esercizio_4/sketch.js
@@ -0,0 +1,23 @@
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	textAlign(CENTER, CENTER);
+	textSize(64);
+  }
+  
+  function draw() {
+	background(200);
+  
+	let snapshot = int(random(1000000));
+	if (mouseIsPressed) {
+	  textSize(32);
+	  fill(255);
+	  textFont('Helvetica');
+	  textStyle(BOLD)
+	  text("there is no such thing as time", width/2, height/2)
+	  ;
+	} else {
+	  textSize(64);
+	  fill(255);
+	  text(snapshot, width/2, height/2);
+	}
+  }
