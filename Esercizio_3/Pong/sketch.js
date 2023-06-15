14315ddfbdc2e620a948b4253e73a458e63c7132
larobernasconi <lara.bernasconi@student.supsi.ch>
2023-06-15 13:39:28 +0200

    Modifica codice pong

--- Esercizio_3/Pong/sketch.js
+++ Esercizio_3/Pong/sketch.js
@@ -3,31 +3,28 @@ let xspeed = 2.8;
 let yspeed = 2.2;
 let xdirection = 1;
 let ydirection = 1;
-let rad = 100
+let rad = 100;
 
 function setup() {
-createCanvas(windowWidth, windowHeight);
-noStroke()
-frameRate(100)
-ellipseMode(RADIUS)
-xpos = width/2
-ypos = height/2
-
+  createCanvas(windowWidth, windowHeight);
+  noStroke();
+  frameRate(60);
+  ellipseMode(RADIUS);
+  xpos = width / 2;
+  ypos = height / 2;
 }
 
 function draw() {
   colorMode(HSB);
 
-  let hue = map(frameCount, 0, 360, 0, 360);
-  let saturation = map(frameCount, 0, 360, 50, 100);
-  let brightness = map(frameCount, 0, 360, 50, 100);
-
-  fill(hue, saturation, brightness);
+  let hue = map(xpos, 0, width, 0, 360); // Adjust hue based on xpos
+  let saturation = map(ypos, 0, height, 50, 100); // Adjust saturation based on ypos
+  let brightness = 100; // Constant brightness
 
   background(200);
-  xpos = xpos + xspeed * xdirection;
-  ypos = ypos + yspeed * ydirection;
-  
+  xpos += xspeed * xdirection;
+  ypos += yspeed * ydirection;
+
   if (xpos > width - rad || xpos < rad) {
     xdirection *= -1;
   }
@@ -35,5 +32,6 @@ function draw() {
     ydirection *= -1;
   }
 
+  fill(hue, saturation, brightness);
   ellipse(xpos, ypos, rad, rad);
 }
